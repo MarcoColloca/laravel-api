3ea49422c57d36bdf1df2c729da079d3fc75ee0b
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-12 14:54:07 +0200

    Add → added index function to  Api/Project Controller

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -3,9 +3,23 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\Project;
 use Illuminate\Http\Request;
 
 class ProjectController extends Controller
 {
-    //
+    public function index(Request $request){
+
+        // creo una variabile per definire gli elementi da inserire in ogni  pagina
+        $per_page = $request->perPage ?? 12;
+
+
+        // Creo una variabile in cui salvo i progetti con le relative relazioni, facendomi ritornare i risultati impaginati
+        $results = Project::with('type', 'technologies')->paginate($per_page);
+
+        // Ritorno i risultati trasformandoli in formato json
+        return response()->json([
+            'results' => $results
+        ]);            
+    }    
 }
