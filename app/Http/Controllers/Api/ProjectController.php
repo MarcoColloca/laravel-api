6650a9597a6374ae1ecb3d186d0ceb4b27d57049
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-13 17:46:09 +0200

    Add → added dynamic route for Single project in api.php and axios json return in Api/ProjectController

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -11,7 +11,7 @@ class ProjectController extends Controller
     public function index(Request $request){
 
         // creo una variabile per definire gli elementi da inserire in ogni  pagina
-        $per_page = $request->perPage ?? 12;
+        $per_page = $request->perPage ?? 6;
 
 
         // Creo una variabile in cui salvo i progetti con le relative relazioni, facendomi ritornare i risultati impaginati
@@ -21,5 +21,19 @@ class ProjectController extends Controller
         return response()->json([
             'results' => $results
         ]);            
-    }    
+    }
+    
+    
+    public function show(Project $project){
+
+
+        $project->load(['type', 'type.projects', 'technologies']);
+
+        $results = $project;
+
+        return response()->json([
+            'results' => $results
+        ]);            
+    
+    }
 }
