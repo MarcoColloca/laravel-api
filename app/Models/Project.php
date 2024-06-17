ff8b2ece40873a0bb7adf5fabe03a9ee8bfc4dde
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-17 12:57:11 +0200

    Add → added function inside Project Model to pass images to frontend

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -3,6 +3,8 @@
 namespace App\Models;
 
 use App\Http\Traits\Sluggable;
+use DateTimeInterface;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
@@ -24,6 +26,22 @@ class Project extends Model
         return $this->belongsToMany(Technology::class);
     }
 
-    protected $fillable = ['name', 'slug', 'link', 'description', 'date_of_creation', 'is_public', 'contributors', 'contributors_link', 'type_id'];
 
+    protected function coverFullPath():Attribute
+    {
+        return new Attribute(
+            get: fn () => $this->cover_image ? asset('http://127.0.0.1:8000/storage/' . $this->cover_image) : null
+        );
+
+    }
+
+    protected function serializeDate(DateTimeInterface $date): string
+    {
+        return $date->format('d-m-Y');
+    }
+
+
+    protected $fillable = ['name', 'slug', 'link', 'description', 'date_of_creation', 'is_public', 'contributors', 'contributors_link', 'type_id', 'cover_image'];
+
+    protected $appends = ['cover_full_path'];
 }
