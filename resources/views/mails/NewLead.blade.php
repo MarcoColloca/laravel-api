c5f3f1dd06799ad20a6cea5b3c015be8ff41c91f
MarcoColloca <marco.colloca3511@gmail.com>
2024-06-18 13:44:08 +0200

    Add → added NewLead view

---
+++ resources/views/mails/NewLead.blade.php
@@ -0,0 +1,17 @@
+<h1>Nuovo Contatto</h1>
+
+
+<p>Nome: {{$lead->name}}</p>
+<p>Email: {{$lead->email}}</p>
+
+<strong>
+    Messaggio:
+</strong>
+<div class="mb-3">
+    {{$lead->message}}
+</div>
+
+
+<div class="py-5">
+    Il nostro team
+</div>
